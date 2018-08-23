a6ead1e4d478d47ee07a98d6fbce02d3de4f54c3
rajeevsathish <rajeev.sathish@tarento.com>
2018-08-23 19:34:06 +0530

    CI Test

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+chmod 0400 /home/travis/build/rajeevsathish/sunbird-devops/deploy/ciTestKey.pem
 ssh -i /home/travis/build/rajeevsathish/sunbird-devops/deploy/ciTestKey.pem -o CheckHostIP=no -o StrictHostKeyChecking=no ubuntu@13.232.188.44
 
 sudo apt-get update -y && sudo apt-get install git -y
