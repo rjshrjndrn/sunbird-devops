0735ace2320518c82b89d7d1bdf4a9eb234051e2
rajeevsathish <rajeev.sathish@tarento.com>
2018-08-23 20:08:45 +0530

    CI Test

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@
 # &&
 # sudo apt-get update -y && sudo apt-get install git -y &&
 # git clone https://github.com/project-sunbird/sunbird-devops.git
-
+chmod 0400 /home/travis/build/rajeevsathish/sunbird-devops/deploy/ciTestKey.pem
 nssh() {
     ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking false" -o "LogLevel ERROR" $@
     return $?
 }
-nssh ubuntu@13.232.188.44 sudo apt-get update -y && sudo apt-get install git -y && git clone https://github.com/project-sunbird/sunbird-devops.git
+nssh -i /home/travis/build/rajeevsathish/sunbird-devops/deploy/ciTestKey.pem ubuntu@13.232.188.44 sudo apt-get update -y && sudo apt-get install git -y && git clone https://github.com/project-sunbird/sunbird-devops.git
