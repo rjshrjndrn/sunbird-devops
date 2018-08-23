5f3dd28fbdc5be57fc63e808f9d49f688b9d67f1
rajeevsathish <rajeev.sathish@tarento.com>
2018-08-23 19:50:07 +0530

    CI Test

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-chmod 0400 /home/travis/build/rajeevsathish/sunbird-devops/deploy/ciTestKey.pem
-ssh -i /home/travis/build/rajeevsathish/sunbird-devops/deploy/ciTestKey.pem -o CheckHostIP=no -o StrictHostKeyChecking=no ubuntu@13.232.188.44
-&&
-sudo apt-get update -y && sudo apt-get install git -y &&
-git clone https://github.com/project-sunbird/sunbird-devops.git
+# chmod 0400 /home/travis/build/rajeevsathish/sunbird-devops/deploy/ciTestKey.pem
+# ssh -i /home/travis/build/rajeevsathish/sunbird-devops/deploy/ciTestKey.pem -o CheckHostIP=no -o StrictHostKeyChecking=no ubuntu@13.232.188.44
+# &&
+# sudo apt-get update -y && sudo apt-get install git -y &&
+# git clone https://github.com/project-sunbird/sunbird-devops.git
+
+ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking false" -o "LogLevel ERROR" -n ubuntu@13.232.188.44 sudo apt-get update -y && sudo apt-get install git -y
