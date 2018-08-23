d84a9f91962144c7e3234a949a43694cfebd933b
rajeevsathish <rajeev.sathish@tarento.com>
2018-08-23 19:04:29 +0530

    CI Test

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ssh -i /home/travis/build/rajeevsathish/sunbird-devops/deploy/ciTestKey.pem ubuntu@13.232.188.44
+
+sudo apt-get update -y && sudo apt-get install git -y
+
+git clone https://github.com/project-sunbird/sunbird-devops.git
