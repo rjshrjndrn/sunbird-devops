d90c420fda2b311c1704c8e32f33a5674e92b5e6
rajeevsathish <rajeev.sathish@tarento.com>
2018-08-29 15:43:49 +0530

    CI Test

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ ssh -tt ubuntu@$dns_name <<EOF
   chmod 0400 ciTestKey.pem
   echo "Sunbir installation starting..."
   echo "INSTALLATION CODE SHOULD COME HERE"
-  ./sunbird_install.sh
+  ./sunbird_install1.sh
   echo "Installation phase ONE complete"
   python /home/ubuntu/getSSOKey.py
   echo "******************************"
