bfa1358d2e32eb8dd32926d6a07e5c3e1cc380e1
rajeevsathish <rajeev.sathish@tarento.com>
2018-09-01 23:52:11 +0530

    CI Testing

--- stage1.sh
+++ stage1.sh
@@ -1,7 +1,8 @@
 set -eu -o pipefail
+sudo add-apt-repository ppa:jonathonf/python-3.6 &&
 sudo apt-get update -y &&
 sudo apt-get install git -y &&
-sudo apt-get install python3.6 &&
+sudo apt-get install python3.6 python3.6 libpython3.6 &&
 pip install pyyaml &&
 git clone $1 &&
 cp config sunbird-devops/deploy &&
